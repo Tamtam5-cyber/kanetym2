3ee04a1f6ca1adedbc410fae06a6361fa2ea72ea
kane5 <Nguyenkhactam5@gmail.com>
2025-02-05 01:08:13 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_HASH = environ.get("API_HASH", "72cc7062951f56abb5240994690e49de")
-API_ID = int(environ.get("API_ID", "25811007"))
-BOT_TOKEN = environ.get("BOT_TOKEN", "7789014828:AAF_85sQOhwProRoVKGuqidjFAlHCQZtUCo")
-BOT_OWNER = int(environ.get("BOT_OWNER", "7240555847"))
-BOT_USERNAME = environ.get("BOT_USERNAME", "tymchomapbot")
+API_HASH = environ.get("API_HASH", "146fcf83a1670e2e926ce874ce31fcb3")
+API_ID = int(environ.get("API_ID", "29762406"))
+BOT_TOKEN = environ.get("BOT_TOKEN", "7027724171:AAGUHwuzSjicKxQagzA19wYr377-4fAM0NI")
+BOT_OWNER = int(environ.get("BOT_OWNER", "6670233911"))
+BOT_USERNAME = environ.get("BOT_USERNAME", "VLXDKIMLY_BOT")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002465408399"))
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002271972643"))
 DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://nguyenkhactam5:q1231234@kanereaction.zhqqd.mongodb.net/?retryWrites=true&w=majority&appName=kanereaction")
